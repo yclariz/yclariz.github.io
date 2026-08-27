e002b915964a626c86d6df3093e5a674d4a0d0f1
yclariz <y.clarizio@gmail.com>
2026-08-27 09:05:46 -0400

    Site commit

---
+++ root/scripts.js
@@ -0,0 +1,4 @@
+/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+function myFunction() {
+    document.getElementById("links").classList.toggle("open");
+}
